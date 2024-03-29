128697447d7000a422616f00f4f4aac8fbaa7daf
Dhia Elhak <126105453+Dhiaelhakkhaldi@users.noreply.github.com>
2024-03-29 16:36:34 +0300

    Add files via upload
    updated with window functions.

--- simpleimplemntaion.py
+++ simpleimplemntaion.py
@@ -1,18 +1,30 @@
 import PySimpleGUI as sg
-
+from converted import convert
 
 label1 = sg.Text('Enter feet:')
-input1 = sg.Input()
+input1 = sg.Input(key='feet')
 
 label2 = sg.Text('Enter inches:')
-input2 = sg.Input()
+input2 = sg.Input(key='inches')
 
 compress_button = sg.Button('Convert')
+output_label = sg.Text('', key='output')
+
 
 window = sg.Window('Convertor', layout=[
                                         [label1, input1],
                                         [label2, input2],
-                                        [compress_button]])
+                                        [compress_button, output_label]])
 
-window.read()
+while True:
+    event, values = window.read()
+
+
+
+    feet = float(values['feet'])
+    inches = float(values['inches'])
+
+    result = convert(feet, inches)
+    window['output'].update(value=f'{result} m', text_color='white')
+
 window.close()
